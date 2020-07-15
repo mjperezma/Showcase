a1a104fc2fbe6b2f747681da9ede9a7cee5f9f79
mj0se <mariajose1907@gmail.com>
2020-07-15 20:14:24 +0200

    new JS code

--- js/main.js
+++ js/main.js
@@ -1,242 +1,146 @@
 'use strict';
 
-let products = [];
-let addProducts = [];
-const productsElement = document.querySelector('.js-cart-items');
-const cartElements = document.getElementById('js-cart');
+// añadir valor al input
 
-// coger los datos que vamos a pintar
+const containerInputs = document.querySelectorAll('.container__price');
 
-const getApiData = () => {
-  fetch('../js/api/data.json')
-    .then((response) => response.json())
-    .then((data) => {
-      products = data.products.items;
-      paintProducts();
-      paintCartElements();
-    });
-};
-
-// pintar los productos en el HTML
-
-const getProductsHtmlCode = (product) => {
-  let htmlCode = '';
-  htmlCode += `<div ondrop='drop(event) ondragover='allowDrop(event)>`;
-  htmlCode += `<article onClick="productInfo()" ondragstart='dragStart(event)' draggable='true' ondragend='dragEnd(event)'  id="${product.id}" class='products__article js-product>'  draggable='true'>`;
-  htmlCode += ` <img class='products__img' src="${product.image}"' alt='${product.name}' />`;
-  htmlCode += `<div class='container__price'>`;
-  htmlCode += ` <h3>${product.name}</h3>`;
-  htmlCode += `<small>${product.price}€/K</small>`;
-  htmlCode += `<button class='btn js-btn' id='${product.id}'>
-      Añadir a la cesta
-    </button>`;
-  htmlCode += `</div>`;
-  htmlCode += `</article>`;
-  htmlCode += `</div>`;
-
-  return htmlCode;
-};
-
-const paintProducts = () => {
-  let productsCode = '';
-  for (const product of products) {
-    productsCode += getProductsHtmlCode(product);
-  }
-
-  productsElement.innerHTML = productsCode;
-  listenAddProductsBtns();
-};
-
-// escuchar los productos clickados
-
-const listenAddProductsBtns = () => {
-  const btns = document.querySelectorAll('.js-btn');
-
-  btns.forEach((btn) => {
-    return btn.addEventListener('click', addProductQuantity);
-  });
-};
-
-// obtener el producto seleccionado
-
-const addProductQuantity = (ev) => {
-  let clickedId = ev.target.id;
-  let foundItem;
-  // comprobamos si el producto está en la cesta
-  for (let item of addProducts) {
-    if (item.id === clickedId) {
-      foundItem = item;
-    }
-  }
-  //   si no está en la cesta
-  if (foundItem === undefined) {
-    let foundProduct;
-    //   buscamos el elemento clickado
-    for (let product of products) {
-      if (product.id === clickedId) {
-        foundProduct = product;
-      }
-    }
-
-    // añado el producto a la cesta
-    addProducts.push({
-      id: foundProduct.id,
-      name: foundProduct.name,
-      price: foundProduct.price,
-      image: foundProduct.image,
-      quantity: 1,
-    });
-  } else {
-    foundItem.quantity += 1;
-  }
-
-  paintCartElements();
-};
-
-//  eliminar productos de la cesta
-
-const decProductQuantity = (ev) => {
-  let clickedId = ev.target.id;
-  let foundItem;
-  for (const item of addProducts) {
-    if (item.id === clickedId) {
-      foundItem = item;
-    }
-  }
-  if (foundItem.quantity > 1) {
-    foundItem.quantity -= 1;
-  } else {
-    let foundIndex;
-    for (let index = 0; index < addProducts.length; index += 1) {
-      if (addProducts[index.id === clickedId]) {
-        foundIndex = index;
-      }
-    }
-    addProducts.splice(foundIndex, 1);
-  }
-  paintCartElements();
-};
-
-// pintar la cesta
-
-const getCartHtmlCode = (item) => {
-  let htmlCodeAdd = '';
-  htmlCodeAdd += `<article class='article__add'>`;
-  htmlCodeAdd += `<h3>${item.name}</h3>`;
-  htmlCodeAdd += `<img class='article__add--img' src=${item.image} alt=${item.name}>`;
-  htmlCodeAdd += `    <button class="article__btn js-dec-item " id=${item.id}>-</button>`;
-  htmlCodeAdd += `    ${item.quantity}`;
-  htmlCodeAdd += `    <button class="article__btn js-add-item" id=${item.id}>+</button>`;
-  htmlCodeAdd += `<span> Total : ${item.price * item.quantity}€ </span>`;
-  htmlCodeAdd += `<button class='btn__delete js-delete' id=${item.id}><i class="fa fa-trash" aria-hidden="true"></i></button>`;
-  htmlCodeAdd += `</article>`;
-
-  return htmlCodeAdd;
-};
-
-// pintar el total de la cesta
-
-const getTotalPriceHtml = () => {
-  let htmlCodeTotal = '';
-  htmlCodeTotal += `<div>`;
-  htmlCodeTotal += `<span class='total__price'>Total: ${getTotalPrice()} €</span>`;
-  htmlCodeTotal += `</div>`;
-  return htmlCodeTotal;
-};
-
-// conseguir el total de la cesta
-
-const getTotalPrice = () => {
+for (let index = 0; index < containerInputs.length; index++) {
+  const containerInput = containerInputs[index];
+  const addValue = containerInput.querySelector('.js-inc');
+  const removeValue = containerInput.querySelector('.js-dec');
   let total = 0;
-  for (const item of addProducts) {
-    total += item.price * item.quantity;
-  }
 
-  return total;
-};
+  addValue.addEventListener('click', () => {
+    if (total < 10) {
+      let value = containerInput.querySelector('.quantity').value;
+      total = Number(value) + 1;
 
-const paintCartElements = () => {
-  cartElements.innerHTML = '';
-  for (const item of addProducts) {
-    cartElements.innerHTML += getCartHtmlCode(item);
-  }
-  cartElements.innerHTML += getTotalPriceHtml();
-  listenItemBtns();
-  listenerDelete();
-};
-
-// escuchar los eventos de la cesta
-const listenItemBtns = () => {
-  const itemAddBtns = document.querySelectorAll('.js-add-item');
-  for (const itemAddBtn of itemAddBtns) {
-    itemAddBtn.addEventListener('click', addProductQuantity);
-  }
-
-  const itemDecBtns = document.querySelectorAll('.js-dec-item');
-
-  for (const itemDecBtn of itemDecBtns) {
-    itemDecBtn.addEventListener('click', decProductQuantity);
-  }
-};
-
-// eliminar elementos de la lista
-const listenerDelete = () => {
-  const deleteBtns = document.querySelectorAll('.js-delete');
-
-  for (const deleteBtn of deleteBtns) {
-    deleteBtn.addEventListener('click', deleteProduct);
-  }
-};
-
-const deleteProduct = (ev) => {
-  let clickedDeleted = ev.target.id;
-  let foundIndex;
-  for (let index = 0; index < addProducts.length; index++) {
-    if (clickedDeleted === addProducts[index].id) {
-      foundIndex = index;
+      containerInput.querySelector('.quantity').value = total;
     }
-    addProducts.splice(foundIndex, 1);
-  }
-  paintCartElements();
+  });
+
+  removeValue.addEventListener('click', () => {
+    if (total > 0) {
+      let value = containerInput.querySelector('.quantity').value;
+      total = Number(value) - 1;
+      containerInput.querySelector('.quantity').value = total;
+    }
+  });
+}
+
+// dragg and drop events
+
+const cart = document.querySelector('.js-cart');
+const cartList = document.querySelector('.js-drag-list');
+const totalProducts = document.querySelector('.js-total');
+const price = document.querySelector('.js-price');
+const products = document.querySelectorAll('.js-product');
+const addCarts = document.querySelectorAll('.js-btn');
+
+const dragStart = (event) => {
+  event.dataTransfer.setData('Text', event.target.id);
 };
 
-// iniciar app
+for (let index = 0; index < products.length; index++) {
+  const product = products[index];
+  product.addEventListener('dragstart', dragStart);
+}
 
-paintCartElements();
-getApiData();
-
-// drag and drop events
-
-const productInfo = () => {
-  console.log('hola', addProducts);
-  for (let addProduct of addProducts) {
-    console.log(addProduct.quantity, addProduct.id);
-  }
-  paintCartElements();
+const dragOver = (event) => {
+  event.preventDefault();
 };
 
-productInfo();
-
-const dragCart = document.querySelector('.js-drag-cart');
-let Id = document.getElementById('js-cart');
-let idDrag = document.getElementById('id.product');
-
-function dragStart(event) {
-  event.dataTransfer.setData('Text/plain', event.target.id);
-  console.log(event.target.id);
-}
-
-function dragEnd(event) {
-  dragCart.classList.add('active');
-}
-
-function allowDrop(event) {
+const dropProducts = (event) => {
   event.preventDefault();
-}
+  const id = event.dataTransfer.getData('text');
+  const product = document.querySelector('#' + id);
+  addToCart(product, id);
+};
 
-function drop(event) {
-  event.preventDefault();
-  var data = event.dataTransfer.getData('Text');
+cart.addEventListener('drop', dropProducts);
+cart.addEventListener('dragover', dragOver);
 
-  event.target.appendChild(document.getElementById(data));
-}
+// add to cart
+
+const addToCart = (id, product) => {
+  let exists = document.querySelectorAll(".js-drag-list .js-product[data-id='" + id + "']");
+
+  if (exists.length > 0) {
+    updateProducts(exists[0]);
+  } else {
+    addCartProduct(product, id);
+  }
+  priceCart();
+};
+
+const addCartProduct = (id, product) => {
+  const cloneProduct = product.cloneNode(true);
+  let quantityValue = cloneProduct.querySelector('.quantity').value;
+  cloneProduct.setAttribute('data-id', id);
+  cloneProduct.setAttribute('class', 'article__add');
+  cloneProduct.setAttribute('data-quantity', quantityValue);
+  cloneProduct.removeAttribute('id');
+
+  const removeDiv = cloneProduct.querySelector('.container__btn');
+  removeDiv.parentNode.removeChild(removeDiv);
+  const removeinfoProduct = cloneProduct.querySelector('.info-product');
+  removeinfoProduct.parentNode.removeChild(removeinfoProduct);
+
+  let articleAdd = document.createElement('span');
+  articleAdd.setAttribute('class', 'quantity');
+  articleAdd.innerHTML = ' x ' + quantityValue;
+  cloneProduct.appendChild(articleAdd);
+  articleAdd = document.createElement('span');
+  articleAdd.setAttribute('class', 'sum');
+  cloneProduct.appendChild(articleAdd);
+
+  let deleteIcon = document.createElement('i');
+  deleteIcon.setAttribute('class', 'fas fa-trash');
+  cloneProduct.appendChild(deleteIcon);
+
+  deleteIcon.addEventListener('click', function () {
+    let itemToDelete = this.parentElement;
+    itemToDelete.parentNode.removeChild(itemToDelete);
+    priceCart();
+  });
+
+  cartList.appendChild(cloneProduct);
+};
+
+// modificar total cesta de la compra
+
+const updateProducts = () => {
+  let quantityProducts = item.getAttribute('data-quantity');
+  let quantityInput = document.querySelector('.quantity').value;
+
+  quantityProducts = parseInt(quantityProducts);
+  quantityProducts = quantityProducts + quantityInput;
+  let spanQuantity = item.querySelectorAll('.quantity');
+  spanQuantity.innerHTML = ' x ' + quantityProducts;
+};
+
+const priceCart = () => {
+  let totalPrice = 0;
+  let totalQuantity = 0;
+  let cartProducts = document.querySelectorAll('.article__add');
+
+  for (let index = 0; index < cartProducts.length; index++) {
+    let cartProduct = cartProducts[index];
+    let quantity = cartProduct.getAttribute('data-quantity');
+    let price = cartProduct.querySelector('.price').textContent;
+    let totalCart = quantity * parseFloat(price);
+    let infoCartProduct = cartProduct.querySelector('span.sum');
+    infoCartProduct.innerHTML = ' =  ' + totalCart + '€';
+
+    totalPrice += totalCart;
+
+    let allProducts = cartProduct.getAttribute('data-quantity');
+    totalQuantity += parseInt(allProducts);
+  }
+
+  let infoPrice = document.querySelector('.cart-price');
+  let infoQuantity = document.querySelector('.js-total');
+
+  infoPrice.innerHTML = `<p> Total a pagar: ${totalPrice}€</p>`;
+  infoQuantity.innerHTML = `<p> Cantidad: ${totalQuantity} KG</p>`;
+};
